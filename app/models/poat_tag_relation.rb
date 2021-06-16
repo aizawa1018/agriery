98ab3ae1bdc6d57f3660130a285f3a17df34fdb1
aizawa1018 <81944327+aizawa1018@users.noreply.github.com>
2021-06-16 11:16:32 +0900

    ユーザー管理機能の実装

---
+++ app/models/poat_tag_relation.rb
@@ -0,0 +1,2 @@
+class PoatTagRelation < ApplicationRecord
+end
---
+++ db/migrate/20210616021133_create_poat_tag_relations.rb
@@ -0,0 +1,10 @@
+class CreatePoatTagRelations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :poat_tag_relations do |t|
+      t.references :post, foreign_key: true
+      t.references :tag, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
