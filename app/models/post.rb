8285e8f2457dd7b733c16c83f7824155ee398697
ryota-1231 <rrrrr.1231@gmail>
2021-08-18 15:41:25 +0900

    モデル実装変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-  has_many   :post_tag_relations, dependent: :destroy
+  has_many   :post_tag_relations, foreign_key: :post_id, dependent: :destroy
   has_many   :tags, through: :post_tag_relations
-  has_many   :comment
+  has_many   :comment, dependent: :destroy
   belongs_to :user
 
   def self.search(search)
--- app/models/posts_form.rb
+++ app/models/posts_form.rb
@@ -1,8 +1,10 @@
 class PostsForm
-
   include ActiveModel::Model
+
+  # 読み書き許可
   attr_accessor :text,:title, :user_id, :name, :post_id, :tag_id
 
+  # バリデーション
   with_options presence: true do
     validates :name
     validates :text
@@ -13,6 +15,7 @@ class PostsForm
   # レコードに値があるかないかでcreate or updateに分岐させる
   delegate :persisted?, to: :post
 
+  # 初期化
   def initialize(attributes = nil, post: Post.new)
     @post = post
     attributes ||= default_attributes
@@ -25,10 +28,18 @@ class PostsForm
 
     ActiveRecord::Base.transaction do
       tags = split_tag_names.map { |posts_tag| Tag.find_or_create_by!(name: posts_tag) }
+      # => Tag.find_or_create_by!(name: posts_tag)
+      # データがない場合
+      # Tag.find_or_create_by!(name: "タグ１")
+      # <Tag ~ id: nil, name :タグ1, created_at: nil, updated_at: nil > => 新しいインスタンス
+
+      # データがある場合
+      # Tag.find_or_create_by!(name: "タグ2")
+      # <Tag ~ id: 1, name :タグ2, created_at: 20210819, updated_at: 20210819 > => データを持ってきてインスタンスを作成する
       post.update!(title: title, text: text, tags: tags, user_id: user_id)
     end
-  rescue ActiveRecord::RecordInvalid
-    false
+    rescue ActiveRecord::RecordInvalid
+      false
   end
 
   def to_model
@@ -48,6 +59,9 @@ class PostsForm
   end
 
   def split_tag_names
+    # name = "タグ1,タグ2,タグ3"
+    # name.split(',')
+    # name = ["タグ1", "タグ2", "タグ3"]
     name.split(',')
   end
 
